4ccaec845d069976f77f70f4542fea6215ab783a
Bakhtiyar_Garashov <bakhtiyar.garashov@mooncascade.com>
2022-03-26 18:58:28 +0200

    Step 2: Geoserver installation steps added

---
+++ scripts/install-geoserver-ubuntu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# ssh to your server and follow the steps below
+
+sudo apt update
+
+# install Java development kit
+
+sudo apt install openjdk-8-jdk
+
+# make sure it is installed
+
+java -version
+
+# create temp folder under current directory
+
+mkdir temp
+
+# cd into
+# use wget to download the GeoServer zip file
+
+wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.20.3/geoserver-2.20.3-bin.zip
+
+# install unzip
+
+sudo apt install unzip
+
+# create main dir for geoserver
+
+mkdir /usr/share/geoserver
+
+# unzip the file
+sudo unzip geoserver-2.20.3-bin.zip -d /usr/share/geoserver
+
+# change owner of files under folder recursively
+# replace 'bakhtiyar' with your username
+
+sudo chown -R bakhtiyar /usr/share/geoserver
+
+#and finally run start bash script
+
+cd bin
+
+./startup.sh
