8d24d1ce992ed9620c2e3db308c41375122389b6
Bakhtiyar_Garashov <bakhtiyar.garashov@mooncascade.com>
2022-03-27 19:39:16 +0300

    Cors issue fix for GCP

--- scripts/upload_pbf_gcp_bucket.sh
+++ scripts/upload_pbf_gcp_bucket.sh
@@ -11,3 +11,22 @@ gsutil -m cp -r soil_12c_drop_denser-8-16 gs://eesti-soil-vtiles
 # URL to retrieve tiles 
 # https://storage.googleapis.com/eesti-soil-vtiles/soil_12c_drop_denser-8-16/{z}/{x}/{y}.pbf
 # no security - public access :)
+
+# After download finished CORS issue will prevent accessing individual tiles from the browser
+
+# Fix CORS issue
+
+# create JSON file locally and add cors rules inside
+
+touch cors.json
+
+echo '[
+    {
+      "origin": ["*"],
+      "method": ["*"]
+    }
+]' > cors.json # allow everything :)
+
+gsutil cors set cors.json gs://eesti-soil-vtiles
+
+# reload browser, move to study area and see tiles
